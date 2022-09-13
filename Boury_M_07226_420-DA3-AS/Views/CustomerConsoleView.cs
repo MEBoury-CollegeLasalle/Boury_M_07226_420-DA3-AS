503c21861b4196d57252006b3206b8d1285f2699
Marc-Eric Boury <meboury@hotmail.com>
2022-09-13 17:39:52 -0400

    console views

--- Boury_M_07226_420-DA3-AS/Views/CustomerConsoleView.cs
+++ Boury_M_07226_420-DA3-AS/Views/CustomerConsoleView.cs
@@ -1,4 +1,5 @@
 ﻿using Boury_M_07226_420_DA3_AS.Models;
+using Boury_M_07226_420_DA3_AS.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +9,16 @@ using System.Threading.Tasks;
 namespace Boury_M_07226_420_DA3_AS.Views {
     internal class CustomerConsoleView : IModelView<Customer> {
 
-
+        public CustomerConsoleView() { 
+        }
 
         public void Render(Customer modelInstance) {
             Console.WriteLine($"Id: {modelInstance.Id} " +
                 $"\nEmail: {modelInstance.Email} " +
                 $"\nFirst Name: {modelInstance.FirstName} " +
-                $"\nLast Name: {modelInstance.LastName}."
+                $"\nLast Name: {modelInstance.LastName} " +
+                $"\nCreated at: {modelInstance.CreatedAt.ToString(DbUtils.DATETIME_DEFAULT_FORMAT)} " +
+                $"\nDeleted at: {modelInstance.DeletedAt.ToString(DbUtils.DATETIME_DEFAULT_FORMAT)}."
             );
         }
     }
