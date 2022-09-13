75f084949c1c048df299efaec32e62b62ffb03bc
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2022-09-12 20:37:26 -0400

    week 4 start

---
+++ Boury_M_07226_420-DA3-AS/Views/CustomerConsoleView.cs
@@ -0,0 +1,21 @@
+﻿using Boury_M_07226_420_DA3_AS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boury_M_07226_420_DA3_AS.Views {
+    internal class CustomerConsoleView : IModelView<Customer> {
+
+
+
+        public void Render(Customer modelInstance) {
+            Console.WriteLine($"Id: {modelInstance.Id} " +
+                $"\nEmail: {modelInstance.Email} " +
+                $"\nFirst Name: {modelInstance.FirstName} " +
+                $"\nLast Name: {modelInstance.LastName}."
+            );
+        }
+    }
+}
